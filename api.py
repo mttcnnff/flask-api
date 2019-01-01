9ab4b99b041d6f30b5d19887d3b2d80b1e1d372a
Matthew Conniff <mvconniff@gmail.com>
2019-01-01 17:56:38 -0500

    Added now method

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,7 +9,14 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+class Now(Resource):
+    def get(self):
+        now = str(datetime.datetime.now())
+        return {'current_datetime': now}
+
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(Now, '/now')
 
 if __name__ == '__main__':
     app.run(debug=True)
